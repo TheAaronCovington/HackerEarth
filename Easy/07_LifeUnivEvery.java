09a4c90b95a9050705ea4fb5434838fbe379f0b0
Aaron Covington <TheAaronCovington@gmail.com>
2015-09-18 19:22:06 -0500

    Create 07_LifeUnivEvery.java

---
+++ Easy/07_LifeUnivEvery.java
@@ -0,0 +1,14 @@
+/*
+  DESCRIPTION: Reads in numbers and prints them out until number 42 is read.  Super easy problem, probably easiest on the site.
+*/
+import java.util.Scanner;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        while(num != 42){
+        	System.out.println(num);
+        	num = input.nextInt();
+        }
+    }
+}
